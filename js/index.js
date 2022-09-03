2385a5eece1f4ed0ec5286b5fc601c1e42575714
Rasel Parvej <RaselParvejABC@gmail.com>
2022-09-03 09:49:53 +0600

    News by Category Fetch Works

--- js/index.js
+++ js/index.js
@@ -1,3 +1,20 @@
+// Document Queries
+const newsArena = document.querySelector("main#news-arena");
+const newsSpinner = newsArena.querySelector("div#news-spinner");
+const foundNumberAndCategoryElement = newsArena.querySelector(
+  "div#number-of-found-news"
+);
+const newsCardsElement = newsArena.querySelector("section#news-cards");
+const numberOfFoundNewsElement =
+  foundNumberAndCategoryElement.querySelector("span:nth-child(1)");
+const foundNewsInCategoryElement =
+  foundNumberAndCategoryElement.querySelector("span:nth-child(2)");
+
+//Show Network Error Modal
+function showNetworkErrorModal() {
+  new bootstrap.Modal("#network-error-modal").show();
+}
+
 // REST API Endpoints
 const listOfCategoriesEndpoint =
   "https://openapi.programming-hero.com/api/news/categories";
@@ -22,15 +39,19 @@ async function fetchData(url) {
     return responseBody["data"];
   } catch (error) {
     console.log(error);
-    new bootstrap.Modal("#network-error-modal").show();
+    showNetworkErrorModal();
     throw error;
   }
 }
+
+// Load Categories from Network
 
 async function loadCategories() {
   const data = await fetchData(listOfCategoriesEndpoint);
   return data["news_category"];
 }
+
+// Display Categories
 
 async function displayCategories() {
   const newsCategoriesElement = document.querySelector(
@@ -43,17 +64,53 @@ async function displayCategories() {
       newsCategoriesElement.insertAdjacentHTML(
         "beforeend",
         `
-          <div class="border rounded" data-id="${category["category_id"]}">
+          <div class="border rounded category" data-id="${category["category_id"]}">
             ${category["category_name"]}
           </div>
         `
       );
     });
+
+    // Registering Click Handlers for .category Elements
+    registerClickHandlerForCategories();
   } catch (error) {
     newsCategoriesElement.innerHTML = `
-            <p class="text-danger fw-bold fs-5">Network Error</p>
+            <p class="text-danger fw-bold fs-5 mt-2">Network Error while Loading Categories</p>
         `;
+  } finally {
+    document.querySelector("#category-spinner").classList.add("d-none");
   }
 }
 
+// Invoke Display Categories
 displayCategories();
+
+//Handling Click on Categories
+function registerClickHandlerForCategories() {
+  const categoryElements = Array.from(
+    document.querySelectorAll("section#news-categories .category")
+  );
+
+  categoryElements.forEach((categoryElement) => {
+    categoryElement.addEventListener("click", async function () {
+      try {
+        newsSpinner.classList.toggle("d-none");
+        foundNumberAndCategoryElement.classList.toggle("d-none");
+        newsCardsElement.classList.toggle("d-none");
+
+        const categoryID = this.dataset.id;
+        const arrayOfNews = await fetchData(
+          `${allNewsOfACategoryEndpoint}${categoryID}`
+        );
+        console.log(arrayOfNews);
+        // After Data Received
+        newsSpinner.classList.toggle("d-none");
+        foundNumberAndCategoryElement.classList.toggle("d-none");
+        newsCardsElement.classList.toggle("d-none");
+        // Setting Number of Found News in the Selected Category
+        numberOfFoundNewsElement.textContent = arrayOfNews.length && "NO";
+        foundNewsInCategoryElement.textContent = this.textContent;
+      } catch (error) {}
+    });
+  });
+}
