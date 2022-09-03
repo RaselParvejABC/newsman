989e7d490c2f74caf346c430b75d200160f7c1b7
Rasel Parvej <RaselParvejABC@gmail.com>
2022-09-03 08:17:45 +0600

    News Categories Fetched and Shown

--- js/index.js
+++ js/index.js
@@ -12,19 +12,48 @@ const detailOfANewsEndpoint = "https://openapi.programming-hero.com/api/news/";
 async function fetchData(url) {
   try {
     const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error("Response Not OK");
+    }
     const responseBody = await response.json();
+    if (!responseBody["status"]) {
+      throw new Error("Status False");
+    }
     return responseBody["data"];
   } catch (error) {
     console.log(error);
     new bootstrap.Modal("#network-error-modal").show();
-    // throw error;
+    throw error;
   }
 }
 
 async function loadCategories() {
   const data = await fetchData(listOfCategoriesEndpoint);
-  const categories = data["news_category"];
-  console.log(categories);
+  return data["news_category"];
 }
 
-loadCategories();
+async function displayCategories() {
+  const newsCategoriesElement = document.querySelector(
+    "section#news-categories"
+  );
+  newsCategoriesElement.innerHTML = "";
+  try {
+    const categories = await loadCategories();
+    categories.forEach((category) => {
+      newsCategoriesElement.insertAdjacentHTML(
+        "beforeend",
+        `
+          <div class="border rounded" data-id="${category["category_id"]}">
+            ${category["category_name"]}
+          </div>
+        `
+      );
+    });
+  } catch (error) {
+    newsCategoriesElement.innerHTML = `
+            <p class="text-danger fw-bold fs-5">Network Error</p>
+        `;
+  }
+}
+
+displayCategories();
